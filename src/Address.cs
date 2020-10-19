e17522921b8ca0944b606a7f2045f614009d69eb
Steve Ellwood <stephen.ellwood@northlincs.gov.uk>
2020-10-19 13:44:30 +0100

    Added Address

---
+++ src/Address.cs
@@ -0,0 +1,172 @@
+//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file=Address.cs company="North Lincolnshire Council">
+//  Solution : -  NLC.Library
+// 
+//  </copyright>
+//  <summary>
+// 
+//  Created - 19/10/2020 12:27
+//  Altered - 19/10/2020 13:30 - Stephen Ellwood
+// 
+//  Project : - NLC.Library
+// 
+//  </summary>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using NLC.Library.Interfaces;
+
+namespace NLC.Library
+    {
+        /// <summary>
+        ///     Full Address
+        /// </summary>
+        /// <remarks>
+        ///     Location, including all address fields
+        /// </remarks>
+        public class Address : AddressBase, IAddressLines, IAddressNameNumber, IAddressNamed
+            {
+                public Address()
+                    {
+                    }
+
+                public Address(IAddressLines address) : base(address)
+                    {
+                    }
+
+                public Address(IAddressNameNumber address) : base(address)
+                    {
+                    }
+
+                public Address(IAddressNamed address) : base(address)
+                    {
+                    }
+
+                /// <inheritdoc />
+                public string AddressLine1
+                {
+                    get => Address1;
+                    set => Address1 = value;
+                }
+
+                /// <inheritdoc />
+                public string AddressLine2
+                {
+                    get => Address2;
+                    set => Address2 = value;
+                }
+
+
+                /// <inheritdoc />
+                public string AddressLine3
+                {
+                    get => Address3;
+                    set => Address3 = value;
+                }
+
+                /// <inheritdoc />
+                public string AddressLine4
+                {
+                    get => Address4;
+                    set => Address4 = value;
+                }
+
+                /// <inheritdoc />
+                public string AddressLine5
+                {
+                    get => Address5;
+                    set => Address5 = value;
+                }
+
+                /// <inheritdoc />
+                public string AddressLine6
+                {
+                    get => Address6;
+                    set => Address6 = value;
+                }
+
+                /// <inheritdoc />
+                public string PrimaryAddress
+                {
+                    get => Address1;
+                    set => Address1 = value;
+                }
+
+                /// <inheritdoc />
+                public string SecondaryAddress
+                {
+                    get => Address2;
+                    set => Address2 = value;
+                }
+
+                public string Street
+                {
+                    get => Address3;
+                    set => Address3 = value;
+                }
+
+                public string Location
+                {
+                    get => Address4;
+                    set => Address4 = value;
+                }
+
+                public string Town
+                {
+                    get => Address5;
+                    set => Address5 = value;
+                }
+
+                public string County
+                {
+                    get => Address6;
+                    set => Address6 = value;
+                }
+
+                public string HouseName
+                {
+                    get => Address1;
+                    set => Address1 = value;
+                }
+
+                /// <inheritdoc />
+                public string HouseNumber
+                {
+                    get => Address2;
+                    set => Address2 = value;
+                }
+
+
+                public AddressLines AddressLines() =>
+                    new AddressLines
+                        {
+                            AddressLine1 = Address1,
+                            AddressLine2 = Address2,
+                            AddressLine3 = Address3,
+                            AddressLine4 = Address4,
+                            AddressLine5 = Address5,
+                            AddressLine6 = Address6
+                        };
+
+                public AddressNamed AddressNamed() =>
+                    new AddressNamed
+                        {
+                            PrimaryAddress = Address1,
+                            SecondaryAddress = Address2,
+                            Street = Address3,
+                            Location = Address4,
+                            Town = Address5,
+                            County = Address6
+                        };
+
+                public AddressNameNumber AddressNameNumber() =>
+                    new AddressNameNumber
+                        {
+                            HouseName = Address1,
+                            HouseNumber = Address2,
+                            Street = Address3,
+                            Location = Address4,
+                            Town = Address5,
+                            County = Address6
+                        };
+            }
+    }
--- tests/AddressTests.cs
+++ tests/AddressTests.cs
@@ -1,18 +1,19 @@
 //  --------------------------------------------------------------------------------------------------------------------
 //  <copyright file=AddressTests.cs company="North Lincolnshire Council">
-//  Solution : -  Library
+//  Solution : -  NLC.Library
 // 
 //  </copyright>
 //  <summary>
 // 
-//  Created - 03/07/2020 17:11
-//  Altered - 06/07/2020 12:54 - Stephen Ellwood
+//  Created - 13/10/2020 16:38
+//  Altered - 19/10/2020 13:41 - Stephen Ellwood
 // 
-//  Project : - Library.tests
+//  Project : - NLC.Library.Tests
 // 
 //  </summary>
 //  --------------------------------------------------------------------------------------------------------------------
 
+using NLC.Library.Interfaces;
 using NUnit.Framework;
 
 namespace NLC.Library.Tests
@@ -20,20 +21,90 @@ namespace NLC.Library.Tests
         /// <summary>
         ///     disparate test object testing
         /// </summary>
-        /// <remarks>
-        ///     This is intended to test comparisons between different objects with the same base class
-        ///     e.g. simpleAddress and SimpleAddressLine
-        /// </remarks>
         [TestFixture]
         public class AddressTests
             {
-                private AddressNameNumber CreateSimpleAddressNameNumber() => new AddressNameNumber();
+                private Address CreateAddress() => new Address();
 
-                private AddressLines CreateSimpleAddressLine() => new AddressLines();
+                private Address PopulatedAddress() => new Address
+                    {
+                        Address1 = "add1",
+                        Address2 = "add2",
+                        Address3 = "add3",
+                        Address4 = "add4",
+                        Address5 = "add5",
+                        Address6 = "add6",
+                    };
 
-                private AddressNamed CreateSimpleAddress() => new AddressNamed();
+                [Test]
+                public void CheckInterfaces()
+                    {
+                        var sut = CreateAddress();
+
+                        Assert.IsInstanceOf<IAddress>(sut);
+                        Assert.IsInstanceOf<ILocation>(sut);
+                        Assert.IsInstanceOf<IAddressLines>(sut);
+                        Assert.IsInstanceOf<IAddressNamed>(sut);
+                        Assert.IsInstanceOf<IAddressNameNumber>(sut);
+                    }
+
+                [Test]
+                public void InterfacesWork()
+                    {
+                        var sut = PopulatedAddress();
+
+                        var actualLines = sut.AddressLines();
+                        var actualNamed = sut.AddressNamed();
+                        var actualNameNumber = sut.AddressNameNumber();
+
+                        Assert.That(actualLines, Is.Not.Null);
+                        Assert.That(actualLines.AddressLine1, Is.EqualTo("add1"));
+                        Assert.That(actualLines.AddressLine2, Is.EqualTo("add2"));
+                        Assert.That(actualLines.AddressLine3, Is.EqualTo("add3"));
+                        Assert.That(actualLines.AddressLine4, Is.EqualTo("add4"));
+                        Assert.That(actualLines.AddressLine5, Is.EqualTo("add5"));
+                        Assert.That(actualLines.AddressLine6, Is.EqualTo("add6"));
 
 
+                        Assert.That(actualNamed, Is.Not.Null);
+                        Assert.That(actualNamed.PrimaryAddress, Is.EqualTo("add1"));
+                        Assert.That(actualNamed.SecondaryAddress, Is.EqualTo("add2"));
+                        Assert.That(actualNamed.Street, Is.EqualTo("add3"));
+                        Assert.That(actualNamed.Location, Is.EqualTo("add4"));
+                        Assert.That(actualNamed.Town, Is.EqualTo("add5"));
+                        Assert.That(actualNamed.County, Is.EqualTo("add6"));
+
+                        Assert.That(actualNameNumber, Is.Not.Null);
+                        Assert.That(actualNameNumber.HouseName, Is.EqualTo("add1"));
+                        Assert.That(actualNameNumber.HouseNumber, Is.EqualTo("add2"));
+                        Assert.That(actualNameNumber.Street, Is.EqualTo("add3"));
+                        Assert.That(actualNameNumber.Location, Is.EqualTo("add4"));
+                        Assert.That(actualNameNumber.Town, Is.EqualTo("add5"));
+                        Assert.That(actualNameNumber.County, Is.EqualTo("add6"));
+                    }
+
+                [Test]
+                public void FieldsReturnExpected()
+                    {
+                        var sut = PopulatedAddress();
+
+                        Assert.That(sut, Is.Not.Null);
+                        Assert.That(sut.AddressLine1, Is.EqualTo("add1"));
+                        Assert.That(sut.AddressLine2, Is.EqualTo("add2"));
+                        Assert.That(sut.AddressLine3, Is.EqualTo("add3"));
+                        Assert.That(sut.AddressLine4, Is.EqualTo("add4"));
+                        Assert.That(sut.AddressLine5, Is.EqualTo("add5"));
+                        Assert.That(sut.AddressLine6, Is.EqualTo("add6"));
+
+                        Assert.That(sut.PrimaryAddress, Is.EqualTo("add1"));
+                        Assert.That(sut.SecondaryAddress, Is.EqualTo("add2"));
+                        Assert.That(sut.Street, Is.EqualTo("add3"));
+                        Assert.That(sut.Location, Is.EqualTo("add4"));
+                        Assert.That(sut.Town, Is.EqualTo("add5"));
+                        Assert.That(sut.County, Is.EqualTo("add6"));
+
+                        Assert.That(sut.HouseName, Is.EqualTo("add1"));
+                        Assert.That(sut.HouseNumber, Is.EqualTo("add2"));
                     }
             }
-    
+    }
